337a36bb961006d8e537953309dcbab512bd7040
Beetroot Paul <paul@beetrootpaul.com>
2024-06-16 11:38:54 +0200

    camera follows the player

---
+++ Assets/Scripts/FollowingCamera.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    [SerializeField] private Transform followedObject;
+    [SerializeField] private Vector3 relativePosition = new(0, 2, -3);
+
+    private void Update()
+    {
+        transform.position = followedObject.transform.position + relativePosition;
+    }
+}
